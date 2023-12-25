09dda3bb4c04470f822e977b094ecd279571aa2d
Harsh Vardhan Gupta <harsh247@gmail.com>
2023-12-25 14:20:26 +0530

    .

--- azad_website/azad_website/settings.py
+++ azad_website/azad_website/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'azad_website'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'azad/static/media')
